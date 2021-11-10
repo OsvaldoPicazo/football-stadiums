e83c254d25fcd8c9444635716f50d0d01ce0247b
Osvaldo Picazo <odpicazo@gmail.com>
2021-11-10 14:33:55 +0100

    first commit. Added vue frontend

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>Football Stadiums around the world</h3>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+});
+</script>
+
+<style>
+  
+</style>
