37c52ca05586e71ab92311f18372d2dad4e2d12f
Osvaldo Picazo <odpicazo@gmail.com>
2021-11-11 19:02:47 +0100

    added instructions

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,6 +1,11 @@
 <template>
     <header>
         <h1> {{title}} </h1>
+        <div class="instructions">
+            <p>- Click on the map to add a new entry</p>
+            <p>- Double click on the stadium box to display a picture</p>
+            <p>- Edit and/or delete markers by clicking on the icons</p>
+        </div>
     </header>
 </template>
 
@@ -22,5 +27,9 @@
         justify-content: space-between;
         align-items: center;
         margin-bottom: 30px;
+    }
+
+    .instructions {
+        font-size: 12px;
     }
 </style>
