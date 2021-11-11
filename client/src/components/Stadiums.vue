56f99709e5dba53bef1dffdf4ac112c368018756
Osvaldo Picazo <odpicazo@gmail.com>
2021-11-11 04:36:18 +0100

    added fetching functionality. added list of stadiums

---
+++ client/src/components/Stadiums.vue
@@ -0,0 +1,63 @@
+<template>
+    <!-- Map stadiums array  -->
+    <div class="stadiums">
+        <div 
+            class="stadium"
+            v-for="stadium in stadiums"
+            v-bind:key="stadium.id"
+        >
+            {{ stadium.name }}
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Stadiums',
+    props: {
+        stadiums: Array
+    }
+})
+</script>
+
+<style scoped>
+    .stadiums {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 30px;
+        margin-bottom: 30px;
+        border-radius: 5px;
+    }
+
+    .stadium {
+        background: lightblue;
+        padding: 1rem;
+        border: 1px solid darkgray;
+        border-radius: 5px;
+        text-align: center;
+        position: relative;
+        font-size: 14px;
+    }
+
+    /* media queries to change the number of columns */
+    @media (max-width: 1000px) {
+        .stadiums {
+            grid-template-columns: repeat(3, 1fr);
+        }
+    }
+
+    @media (max-width: 640px) {
+        .stadiums {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+    
+    @media (max-width: 400px) {
+        .stadiums {
+            grid-template-columns: 1fr;
+        }
+    }    
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,41 @@
 <template>
-    <Map />
+    <Map :stadiums="stadiums" />
+    <Stadiums :stadiums="stadiums" />
 </template>
 
 <script lang="ts">
     import { defineComponent } from 'vue';
+    import axios from "axios";
     import Map from '../components/Map.vue'
+    import Stadiums from '../components/Stadiums.vue'
 
     export default defineComponent({
         name: 'Home',
         components: {
-            Map
+            Map,
+            Stadiums
+        },
+        data() {
+            return {
+                // array to render
+                stadiums: []
+            }
+        },
+        methods: {
+            // fetch data from api
+            async fetchStadiums() {
+                try {
+                    const res = await axios.get('http://localhost:5000/stadiums')
+                    this.stadiums = res.data
+                }
+                catch(error) {
+                    console.log("Error while fetching stadiums data: ", error)
+                }
+            }
+        },
+        // fetch data everytime the component is created/page reloaded
+        created() {
+            this.fetchStadiums()
         }
     });
 </script>
